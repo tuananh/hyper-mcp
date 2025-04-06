0b3aa54e411ae61111b837b7b7db2b0bc7f903a9
Tuan Anh Tran <me@tuananh.org>
2025-04-06 16:36:18 +0700

    plugin/hash: use json macro for better readability
    Signed-off-by: Tuan Anh Tran <me@tuananh.org>

--- examples/plugins/hash/src/lib.rs
+++ examples/plugins/hash/src/lib.rs
@@ -3,11 +3,9 @@ mod pdk;
 use base64::Engine;
 use extism_pdk::*;
 use pdk::types::*;
-use serde_json::{Map, Value};
-use sha2::{Sha256, Sha512, Sha384, Sha224, Digest};
+use serde_json::json;
 use sha1::Sha1;
-use md5;
-use base32;
+use sha2::{Digest, Sha224, Sha256, Sha384, Sha512};
 
 // Called when the tool is invoked.
 pub(crate) fn call(input: CallToolRequest) -> Result<CallToolResult, Error> {
@@ -20,48 +18,45 @@ pub(crate) fn call(input: CallToolRequest) -> Result<CallToolResult, Error> {
 
     let data = match args.get("data") {
         Some(v) => v.as_str().unwrap(),
-        None => return Err(Error::msg("`data` must be available")),
+        None => return Err(Error::msg("`data` is required")),
     };
 
-    let algorithm = args.get("algorithm")
-        .and_then(|v| v.as_str())
-        .unwrap_or("base64");
+    let algorithm = match args.get("algorithm") {
+        Some(v) => v.as_str().unwrap(),
+        None => return Err(Error::msg("`algorithm` is required")),
+    };
 
     let result = match algorithm {
         "sha256" => {
             let mut hasher = Sha256::new();
             hasher.update(data.as_bytes());
             format!("{:x}", hasher.finalize())
-        },
+        }
         "sha512" => {
             let mut hasher = Sha512::new();
             hasher.update(data.as_bytes());
             format!("{:x}", hasher.finalize())
-        },
+        }
         "sha384" => {
             let mut hasher = Sha384::new();
             hasher.update(data.as_bytes());
             format!("{:x}", hasher.finalize())
-        },
+        }
         "sha224" => {
             let mut hasher = Sha224::new();
             hasher.update(data.as_bytes());
             format!("{:x}", hasher.finalize())
-        },
+        }
         "sha1" => {
             let mut hasher = Sha1::new();
             hasher.update(data.as_bytes());
             format!("{:x}", hasher.finalize())
-        },
+        }
         "md5" => {
             format!("{:x}", md5::compute(data))
-        },
-        "base32" => {
-            base32::encode(base32::Alphabet::RFC4648 { padding: true }, data.as_bytes())
-        },
-        "base64" | _ => {
-            base64::engine::general_purpose::STANDARD.encode(data)
         }
+        "base32" => base32::encode(base32::Alphabet::RFC4648 { padding: true }, data.as_bytes()),
+        "base64" | _ => base64::engine::general_purpose::STANDARD.encode(data),
     };
 
     Ok(CallToolResult {
@@ -77,68 +72,25 @@ pub(crate) fn call(input: CallToolRequest) -> Result<CallToolResult, Error> {
 }
 
 pub(crate) fn describe() -> Result<ListToolsResult, Error> {
-    /*
-    { tools: [{
-        name: "base64",
-        description: "base64 encode data",
-        inputSchema: {
-          type: "object",
-          properties: {
-            data: {
-              type: "string",
-              description: "data to convert to base64",
-            },
-            algorithm: {
-              type: "string",
-              description: "algorithm to use for hashing",
-              enum: ["sha256", "sha512", "md5", "base64"],
-            },
-          },
-          required: ["data", "algorithm"],
-        }]
-    }
-    */
-    let mut data_prop: Map<String, Value> = Map::new();
-    data_prop.insert("type".into(), "string".into());
-    data_prop.insert(
-        "description".into(),
-        "data to convert to hash or encoded format".into(),
-    );
-
-    let mut algorithm_prop: Map<String, Value> = Map::new();
-    algorithm_prop.insert("type".into(), "string".into());
-    algorithm_prop.insert(
-        "description".into(),
-        "algorithm to use for hashing or encoding".into(),
-    );
-    algorithm_prop.insert(
-        "enum".into(),
-        Value::Array(vec![
-            "sha256".into(),
-            "sha512".into(),
-            "sha384".into(),
-            "sha224".into(),
-            "sha1".into(),
-            "md5".into(),
-            "base32".into(),
-            "base64".into(),
-        ])
-    );
-
-    let mut props: Map<String, Value> = Map::new();
-    props.insert("data".into(), data_prop.into());
-    props.insert("algorithm".into(), algorithm_prop.into());
-
-    let mut schema: Map<String, Value> = Map::new();
-    schema.insert("type".into(), "object".into());
-    schema.insert("properties".into(), Value::Object(props));
-    schema.insert("required".into(), Value::Array(vec!["data".into()]));
-
     Ok(ListToolsResult {
         tools: vec![ToolDescription {
             name: "hash".into(),
-            description: "Hash data using various algorithms or encode using base32/base64".into(),
-            input_schema: schema,
+            description: "Hash data using various algorithms:  sha256, sha512, sha384, sha224, sha1, md5, base32, base64".into(),
+            input_schema: json!({
+                "type": "object",
+                "properties": {
+                    "data": {
+                        "type": "string",
+                        "description": "data to convert to hash or encoded format"
+                    },
+                    "algorithm": {
+                        "type": "string",
+                        "description": "algorithm to use for hashing or encoding",
+                        "enum": ["sha256", "sha512", "sha384", "sha224", "sha1", "md5", "base32", "base64"]
+                    }
+                },
+                "required": ["data", "algorithm"]
+            }).as_object().unwrap().clone(),
         }],
     })
 }
